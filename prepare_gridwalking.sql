81c64cad5dca7a1cdcc4a60d8198fba775cdbc50
Frode Roxrud Gill <frodegill@gmail.com>
2017-01-09 20:56:49 +0100

    Added script for preparing database

---
+++ prepare_gridwalking.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS gridwalking DEFAULT CHARACTER SET utf8 COLLATE utf8_danish_ci;
+GRANT ALL PRIVILEGES ON gridwalking.* TO gridwalking@localhost IDENTIFIED BY 'gridwalking';
+FLUSH PRIVILEGES;
