1258fcadfb211017c0763daf1675f38cd4ec86f9
Frode Roxrud Gill <frodegill@gmail.com>
2017-04-03 18:20:45 +0200

    Added column for bonus

--- upgrade_gridwalking.sql
+++ upgrade_gridwalking.sql
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS `gridwalking`.`user` (
   `l2` INT UNSIGNED NOT NULL,
   `l1` INT UNSIGNED NOT NULL,
   `l0` INT UNSIGNED NOT NULL,
+  `bonus` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `score_idx` (`score` ASC),
   UNIQUE INDEX `guid_UNIQUE` (`guid` ASC))
@@ -28,8 +29,8 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_danish_ci;
 
-INSERT INTO user (guid, username, score, l13, l12, l11, l10, l9, l8, l7, l6, l5, l4, l3, l2, l1, l0)
-SELECT u.id, u.username, u.score, u.l13, u.l12, u.l11, u.l10, u.l9, u.l8, u.l7, u.l6, u.l5, u.l4, u.l3, u.l2, u.l1, u.l0 
+INSERT INTO user (guid, username, score, l13, l12, l11, l10, l9, l8, l7, l6, l5, l4, l3, l2, l1, l0, bonus)
+SELECT u.id, u.username, u.score, u.l13, u.l12, u.l11, u.l10, u.l9, u.l8, u.l7, u.l6, u.l5, u.l4, u.l3, u.l2, u.l1, u.l0, 0
 FROM user_tmp u;
 
 DROP TABLE user_tmp;
